d48b468b6f70ca13bf04d6175556e7f73e892179
D. Ip <ipkinghangdesmond@gmail.com>
2023-03-24 19:12:46 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt update
-apt install wireguard tmux vim git zsh neofetch netcat curl mc
+apt install wireguard tmux vim git zsh neofetch netcat curl mc wget
 apt install dnsmasq nftables
 
