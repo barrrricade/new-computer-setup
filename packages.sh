ae59b75e5d7f4b06a748415af9470d8a528537ab
D. Ip <ipkinghangdesmond@gmail.com>
2024-02-24 08:26:17 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,5 +2,7 @@
 
 apt update
 apt install wireguard tmux vim git zsh neofetch netcat curl mc wget
-apt install dnsmasq nftables screen 
+apt install dnsmasq nftables screen openresolv
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
