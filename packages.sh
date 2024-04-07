e1f295aa0779264026d98ef1cb7c73530fb24dbf
D. Ip <ipkinghangdesmond@gmail.com>
2024-04-07 10:39:51 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 apt update
-apt install wireguard tmux vim git zsh neofetch netcat curl mc wget
-apt install dnsmasq nftables screen openresolv
+apt install -y wireguard tmux vim git zsh neofetch netcat curl mc wget
+apt install -y dnsmasq nftables screen openresolv docker.io 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
