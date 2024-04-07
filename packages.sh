f42d0930e302409b373fbfa82be101e7dbc1702c
D. Ip <ipkinghangdesmond@gmail.com>
2024-04-07 10:56:10 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,3 +6,11 @@ apt install -y dnsmasq nftables screen openresolv docker.io
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+wget -O install-docker.sh https://gitlab.com/bmcgonag/docker_installs/-/raw/main/install_docker_nproxyman.sh
+
+chmod +x ./install-docker.sh
+
+./install-docker.sh
+
